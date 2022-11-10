a9a3463c1a640265f69f65cdc3c7770b7595d7a8
AlinaF <afassahova9@gmail.com>
2022-11-11 04:01:15 +0500

    add test10

--- src/test/java/ru/netology/collections/GameTest.java
+++ src/test/java/ru/netology/collections/GameTest.java
@@ -3,8 +3,6 @@ package ru.netology.collections;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GameTest {
 
     @Test
@@ -46,11 +44,36 @@ class GameTest {
         game.register(player1);
         game.register(player2);
 
-        int actual=game.round("ALIK","ALI");
-        int expected=0;
+        int actual = game.round("ALIK", "ALI");
+        int expected = 0;
 
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldNotFirstPlayerStreng() {
+        Game game = new Game();
+        Player player1 = new Player(1, "ALIK", 70);
+        Player player2 = new Player(2, "ALI", 70);
+
+        game.register(player1);
+        game.register(player2);
+
+
+        Assertions.assertThrows(RuntimeException.class, () -> game.round("ALIK1", "ALI"));
+    }
+
+    @Test
+    public void shouldNotSecondPlayerStreng() {
+        Game game = new Game();
+        Player player1 = new Player(1, "ALIK", 70);
+        Player player2 = new Player(2, "ALI", 70);
+
+        game.register(player1);
+        game.register(player2);
+
+
+        Assertions.assertThrows(RuntimeException.class, () -> game.round("ALIK", "ALI2"));
+    }
 
 }
